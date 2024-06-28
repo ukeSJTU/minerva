f16ce0678e683483cde62213c2e0f89d0c2ca5c5
uke_mba <uke_sjtu@sjtu.edu.cn>
2024-06-28 10:28:41 +0800

    feat(ui): add more contents to profile card component

--- src/components/profile_card.tsx
+++ src/components/profile_card.tsx
@@ -1,13 +1,68 @@
-import { AvatarImage, Avatar, AvatarFallback } from "./ui/avatar";
-import { Card } from "./ui/card";
+import React from "react";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { GithubIcon, MailIcon, RssIcon } from "lucide-react";
 
-const ProfileCard = () => {
+interface ProfileCardProps {
+  avatarUrl: string;
+  name: string;
+  motto: string;
+  yearsWriting: number;
+  followers: number;
+  posts: number;
+}
+
+const ProfileCard: React.FC<ProfileCardProps> = ({
+  avatarUrl,
+  name,
+  motto,
+  yearsWriting,
+  followers,
+  posts,
+}) => {
   return (
-    <Card className="flex flex-col md:flex-row items-center bg-card text-card-foreground shadow-sm rounded-xl overflow-hidden w-full max-w-[800px]">
-      <Avatar>
-        <AvatarImage src="/images/avatar.jpg" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+    <Card className="w-[300px]">
+      <CardHeader className="flex flex-col items-center">
+        <Avatar className="w-24 h-24">
+          <AvatarImage src={avatarUrl} alt={name} />
+          <AvatarFallback>{name[0]}</AvatarFallback>
+        </Avatar>
+        <h2 className="mt-4 text-xl font-bold">{name}</h2>
+        <p className="text-sm text-gray-500">{motto}</p>
+      </CardHeader>
+      <CardContent>
+        <div className="flex justify-around text-sm text-gray-500">
+          {/* TODO: need to add clickable links to following contents. Example: click on followers to see all followers */}
+          <div className="flex flex-col justify-between items-center">
+            <span>{yearsWriting}</span>
+            <span>years</span>
+          </div>
+          <div className="flex flex-col justify-between items-center">
+            <span>{followers}</span>
+            <span>followers</span>
+          </div>
+          <div className="flex flex-col justify-between items-center">
+            <span>{posts}</span>
+            <span>posts</span>
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className="flex flex-col items-center">
+        <Button className="w-full mb-4">Subscribe</Button>
+        <div className="flex space-x-4">
+          {/* TODO: need to add clickable external links to following icons. Example: click on github icon and jump to github profile/repo page */}
+          <GithubIcon className="w-5 h-5" />
+          {/* TODO: This GithubIcon will be deprecated */}
+          <MailIcon className="w-5 h-5" />
+          <RssIcon className="w-5 h-5" />
+        </div>
+      </CardFooter>
     </Card>
   );
 };
