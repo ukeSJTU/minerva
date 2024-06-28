e2bbec1e4aac38523ea8935cc6bb88f69bc9bd55
uke_mba <uke_sjtu@sjtu.edu.cn>
2024-06-28 10:37:23 +0800

    feat(ui): :sparkles: use homepage banner in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import { HomepageBanner } from "@/components/banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <HomepageBanner
+          imageUrl="/images/banner_1.jpg"
+          title="Welcome to My Blog"
+          quote="A blog about web development and programming."
+        />
         <div className="pt-20">{children}</div>
       </body>
     </html>
