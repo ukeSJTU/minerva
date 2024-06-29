587d383bf1092cc074367fd2c895c98f422f7f13
uke_mba <uke_sjtu@sjtu.edu.cn>
2024-06-29 16:38:20 +0800

    feat(ui): replace dynamic classname for customed heading component

--- src/components/markdown/heading.tsx
+++ src/components/markdown/heading.tsx
@@ -2,12 +2,31 @@ import React from "react";
 
 interface HeadingProps {
   level: 1 | 2 | 3 | 4 | 5 | 6;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 const Heading: React.FC<HeadingProps> = ({ level, children }) => {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
-  const className = `text-${4 - level}xl font-bold mt-${level} mb-${level - 1}`;
+  let className = "font-bold";
+
+  switch (level) {
+    case 1:
+      className += " text-3xl mt-6 mb-4";
+      break;
+    case 2:
+      className += " text-2xl mt-5 mb-3";
+      break;
+    case 3:
+      className += " text-xl mt-4 mb-2";
+      break;
+    case 4:
+      className += " text-lg mt-3 mb-2";
+      break;
+    case 5:
+    case 6:
+      className += " text-base mt-2 mb-1";
+      break;
+  }
 
   return <Tag className={className}>{children}</Tag>;
 };
