41255f2b384b7bfc4fdf3df6ec9a874618361148
uke_mba <uke_sjtu@sjtu.edu.cn>
2024-06-30 09:39:12 +0800

    feat(ui): :sparkles: integrate top loading bar into page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader />
         <Navbar />
         <div className="pt-[70px]">{children}</div>
       </body>
