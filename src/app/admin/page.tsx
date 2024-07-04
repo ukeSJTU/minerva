90c3776ebc292e14027683d6483ef4e63098f4f8
uke_mba <uke_sjtu@sjtu.edu.cn>
2024-07-04 23:33:03 +0800

    feat(auth): :sparkles: use next-auth to control admin page authorization

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,70 +1,13 @@
-"use client";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import AdminDashboard from "./AdminDashboard";
 
-import { useState, useEffect } from "react";
-import {
-  InputOTP,
-  InputOTPGroup,
-  InputOTPSlot,
-} from "@/components/ui/input-otp";
-import { Button } from "@/components/ui/button";
-import AdminDashboard from "@/app/admin/AdminDashboard";
+export default async function AdminPage() {
+  const session = await auth();
 
-const ADMIN_PASSWORD = "123456";
-
-export default function AdminPage() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [value, setValue] = useState("");
-
-  useEffect(() => {
-    const auth = localStorage.getItem("adminAuth");
-    if (auth === "true") {
-      setIsAuthenticated(true);
-    }
-  }, []);
-
-  const handleSubmit = () => {
-    if (value === ADMIN_PASSWORD) {
-      setIsAuthenticated(true);
-      localStorage.setItem("adminAuth", "true");
-    } else {
-      alert("Incorrect password");
-      setValue("");
-    }
-  };
-
-  if (isAuthenticated) {
-    return <AdminDashboard />;
+  if (!session || !session.user?.isAdmin) {
+    redirect("/api/auth/signin?callbackUrl=/admin");
   }
 
-  return (
-    <div className="container mx-auto py-8 flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-4">Admin Authentication</h1>
-      <div className="space-y-4">
-        <InputOTP
-          maxLength={6}
-          value={value}
-          onChange={(value) => setValue(value)}
-        >
-          <InputOTPGroup>
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
-          </InputOTPGroup>
-        </InputOTP>
-        <div className="text-center text-sm">
-          {value === "" ? (
-            <>Enter the admin password.</>
-          ) : (
-            <>You entered: {value}</>
-          )}
-        </div>
-        <Button onClick={handleSubmit} className="w-full">
-          Submit
-        </Button>
-      </div>
-    </div>
-  );
+  return <AdminDashboard />;
 }
